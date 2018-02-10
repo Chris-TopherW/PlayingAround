a4c057cfe8df35f2e1fb04582645b316136d3942
Chris-TopherW <wrattchri1@AD.ECS.VUW.AC.NZ>
2018-02-10 16:52:00 +1300

    Completely broken but interesting

--- Assets/Scripts/PluginHost/PluginHost.cs
+++ Assets/Scripts/PluginHost/PluginHost.cs
@@ -6,11 +6,15 @@ public class PluginHost : MonoBehaviour
     // The imported function
     [DllImport("VSTHostUnity", EntryPoint = "TestSort")]
     public static extern void TestSort(int[] a, int length);
+    [DllImport("VSTHostUnity", EntryPoint = "loadPlugin", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void loadPlugin(string filepath);
 
     public int[] a;
 
     void Start()
     {
         TestSort(a, a.Length);
+        loadPlugin(Application.dataPath + "/Assets/Data/JuceDemoPlugin.dll");
+        //loadPlugin(0);
     }
 }
